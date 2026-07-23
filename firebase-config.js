4c91791bf24db6dfcebffdaa58224a4bb881306d
tshepzamk-js <tshepzamakwena@gmail.com>
2026-07-23 22:22:47 +0200

    update

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,25 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase-config.js
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// 1. Import the modules directly from Google's official CDN
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js"; // Added Firestore
+
+// 2. Your web app's Firebase configuration
 const firebaseConfig = {
-apiKey: "AIzaSyDjx8V9l6EXNPPNoGsMhUk8PlSHIsIUFcE",
-authDomain: "scaling-train-54894.firebaseapp.com",
-databaseURL: "https://scaling-train-54894-default-rtdb.firebaseio.com",
-projectId: "scaling-train-54894",
-storageBucket: "scaling-train-54894.firebasestorage.app",
-messagingSenderId: "811725820748",
-appId: "1:811725820748:web:7ba2cefe1d287eaf06f5e9",
-measurementId: "G-4S3WX6P18L"
+  apiKey: "AIzaSyDjx8V9l6EXNPPNoGsMhUk8PlSHIsIUFcE",
+  authDomain: "scaling-train-54894.firebaseapp.com",
+  databaseURL: "https://scaling-train-54894-default-rtdb.firebaseio.com",
+  projectId: "scaling-train-54894",
+  storageBucket: "scaling-train-54894.firebasestorage.app",
+  messagingSenderId: "811725820748",
+  appId: "1:811725820748:web:7ba2cefe1d287eaf06f5e9",
+  measurementId: "G-4S3WX6P18L"
 };
 
-// Initialize Firebase
+// 3. Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// 4. Initialize services and EXPORT them so other files can import them
+export const analytics = getAnalytics(app);
+export const db = getFirestore(app); // 'db' represents your Firestore database instance
