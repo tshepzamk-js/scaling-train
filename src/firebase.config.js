b0b4f6d9af1b4b77e11a9ca767524697bb3550ed
tshepzamk-js <tshepzamakwena@gmail.com>
2026-07-23 07:58:42 +0200

    update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,9 +1,11 @@
-// firebase.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDjx8V9l6EXNPPNoGsMhUk8PlSHIsIUFcE",
   authDomain: "scaling-train-54894.firebaseapp.com",
@@ -15,14 +17,6 @@ const firebaseConfig = {
   measurementId: "G-6NMV29P99S"
 };
 
-// Initialize Firebase Core
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize & Export Analytics (Only works in browser environment)
-export const analytics = typeof window !== "undefined" ? getAnalytics(app) : null;
-
-// Initialize & Export services for Astra Academy
-export const auth = getAuth(app);         // For user login / authentication
-export const db = getFirestore(app);      // For Firestore database (subjects, notes, tasks)
-
-export default app;
+const analytics = getAnalytics(app);
